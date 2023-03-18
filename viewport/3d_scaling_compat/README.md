37aab553851c5b15c9d6a9bd4756610bddb66ebe
Ryan Roden-Corrent <ryan@rcorre.net>
2023-03-18 07:42:58 -0400

    Update viewport scaling demo to 4.0.
    Fixes #856.
    
    The 3d_scaling demo was updated to use the built-in 3D scaling mechanisms
    in the Forward+ renderer.
    
    A new 3d_scaling_compat demo was added to demonstrate the legacy
    SubViewport-based scaling method for the Compatibility renderer.

---
+++ viewport/3d_scaling_compat/README.md
@@ -0,0 +1,21 @@
+# 3D Viewport Scaling for Compatibility Renderer
+
+This demo shows how to scale the 3D viewport rendering without affecting 2D
+elements such as the HUD. It also demonstrates how to toggle filtering on a
+viewport. This technique can be useful in 2D games as well. For instance, it can
+be used to have a "pixel art" viewport for the main game area and a
+non-pixel-art viewport for HUD elements.
+
+If you're using the `Forward+` renderer, see ../3d_scaling instead.
+
+Language: GDScript
+
+Renderer: Compatibility
+
+Check out this demo on the asset library: https://godotengine.org/asset-library/asset/586
+
+## Screenshots
+
+![Screenshot](screenshots/high.png)
+
+![Screenshot](screenshots/low.png)
--- viewport/3d_scaling_compat/hud.gd
+++ viewport/3d_scaling_compat/hud.gd
@@ -4,22 +4,23 @@ extends Control
 # 2 is half resolution and 4 is quarter resolution. Lower values look
 # sharper but are slower to render.
 var scale_factor = 1
-var filter_mode = Viewport.SCALING_3D_MODE_BILINEAR
+var filter_mode = TEXTURE_FILTER_NEAREST
 
-@onready var viewport = get_tree().root
+@onready var viewport_container = $SubViewportContainer
+@onready var viewport = $SubViewportContainer/SubViewport
 @onready var scale_label = $VBoxContainer/Scale
 @onready var filter_label = $VBoxContainer/Filter
 
 func _ready():
-	viewport.scaling_3d_mode = Viewport.SCALING_3D_MODE_BILINEAR
+	viewport_container.texture_filter = CanvasItem.TEXTURE_FILTER_LINEAR
 
 func _unhandled_input(event):
 	if event.is_action_pressed("cycle_viewport_resolution"):
 		scale_factor = wrapi(scale_factor + 1, 1, 5)
-		viewport.scaling_3d_scale = 1.0 / scale_factor
+		viewport_container.stretch_shrink = scale_factor
 		scale_label.text = "Scale: %3.0f%%" % (100.0 / scale_factor)
 
 	if event.is_action_pressed("toggle_filtering"):
-		filter_mode = wrapi(filter_mode + 1, Viewport.SCALING_3D_MODE_BILINEAR, Viewport.SCALING_3D_MODE_MAX) as Viewport.Scaling3DMode
-		viewport.scaling_3d_mode = filter_mode
-		filter_label.text = ClassDB.class_get_enum_constants("Viewport", "Scaling3DMode")[filter_mode].capitalize()
+		filter_mode = wrapi(filter_mode + 1, TEXTURE_FILTER_LINEAR, TEXTURE_FILTER_MAX) as TextureFilter
+		viewport_container.texture_filter = filter_mode
+		filter_label.text = ClassDB.class_get_enum_constants("CanvasItem", "TextureFilter")[filter_mode].capitalize()
