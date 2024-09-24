5b0d862c47f2c16e0db0a51e3b5c3c78890fc97a
Allen Pestaluky <allenpestaluky@gmail.com>
2024-09-24 12:00:04 -0400

    Added exponential view mode to the gradients scene. This is disabled by default.

--- 3d/tonemap_color_correction/gradients/gradients_controls.gd
+++ 3d/tonemap_color_correction/gradients/gradients_controls.gd
@@ -1,8 +1,9 @@
-extends HBoxContainer
+extends Node
 
 @export var bars: Array[NodePath]
 @export var max_value_label: Label3D
 @export var colors: Array[Color]
+@export var custom_bar: GradientBars
 
 func _ready():
 	for i in range(colors.size()):
@@ -12,6 +13,8 @@ func _ready():
 		bar.set_color(col)
 
 	_on_steps_value_changed(6)
+	_on_color_picker_button_color_changed(Color(0.5, 0.5, 0.5, 1))
+	_on_exponential_toggled(false)
 
 func _on_steps_value_changed(value):
 	max_value_label.text = "%.1f" % value
@@ -19,3 +22,17 @@ func _on_steps_value_changed(value):
 		var bar = get_node(bar_path)
 		var shader_mat = bar.hdr_bar.material_override as ShaderMaterial
 		shader_mat.set_shader_parameter("steps", value)
+
+
+func _on_color_picker_button_color_changed(color):
+	custom_bar.set_color(color)
+
+
+func _on_exponential_toggled(button_pressed):
+	for bar_path in bars:
+		var bar = get_node(bar_path)
+		var shader_mat = bar.hdr_bar.material_override as ShaderMaterial
+		shader_mat.set_shader_parameter("exponential_view", button_pressed)
+
+		shader_mat = bar.sdr_bar.material_override as ShaderMaterial
+		shader_mat.set_shader_parameter("exponential_view", button_pressed)
